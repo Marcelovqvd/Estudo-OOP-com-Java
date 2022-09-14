c6c5ffbf08a774883cf8bc9a5cf31f28fe8deba1
Marcelo <marcelovqvd@gmail.com>
2022-09-13 22:41:44 -0300

    Test

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,19 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc= new Scanner(System.in);
+
+        double x;
+        System.out.println("Enter x");
+        x = sc.nextDouble();
+        System.out.println(x * 2);
+
+
+        sc.close();
+    }
 }
